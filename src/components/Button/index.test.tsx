24ccb6e5cf53aa1cb136eaaf79a594ab1e922038
Malza0408 <parkjh04080@gmail.com>
2022-12-15 01:09:15 +0900

    [test] checkbox
    checkbox 상태에 따라 버튼의 disabled 여부가 바뀌는지 체크

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -16,3 +16,23 @@ test("button turns blue when clicked", () => {
   fireEvent.click(colorButton);
   expect(colorButton).toHaveStyle("backgroundColor: blue");
 });
+
+test("initial conditions", () => {
+  render(<Button />);
+  const colorButton = screen.getByRole("button", { name: /click!/i });
+  expect(colorButton).toBeEnabled();
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("check button enabled when click checkbox", () => {
+  render(<Button />);
+  const checkbox = screen.getByRole("checkbox");
+  const colorButton = screen.getByRole("button", { name: /click!/i });
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
