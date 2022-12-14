ec8f408733a2c0fa5cfdb96ce984faaba3f449b5
Malza0408 <parkjh04080@gmail.com>
2022-12-15 01:05:47 +0900

    [Add] checkbox상태에 빠른 버튼 활성화/비활성화

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,24 @@ import React, { useState } from "react";
 
 function Button() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [isChecked, setIsChecked] = useState(false);
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
-    <button
-      style={{ backgroundColor: buttonColor }}
-      onClick={() => setButtonColor(newButtonColor)}
-    >
-      Click!
-    </button>
+    <>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={isChecked}
+      >
+        Click!
+      </button>
+      <input
+        type="checkbox"
+        defaultChecked={isChecked}
+        onChange={(e) => setIsChecked(e.target.checked)}
+      />
+    </>
   );
 }
 
