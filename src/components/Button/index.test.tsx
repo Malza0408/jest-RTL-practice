c9558d81996023c0ff49800c4531050db6359e5c
Malza0408 <parkjh04080@gmail.com>
2022-12-14 23:50:58 +0900

    [Add] Button test
    Button이 click되면 색상이 바뀌는지 테스트한다

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { logRoles } from "@testing-library/react";
 import Button from ".";
 
@@ -7,4 +7,12 @@ test("button has red color", () => {
   logRoles(container);
   const buttonElement = screen.getByRole("button", { name: /click!/i });
   expect(buttonElement).toHaveStyle("backgroundColor: red");
+});
+
+test("button turns blue when clicked", () => {
+  render(<Button />);
+  const colorButton = screen.getByRole("button", { name: /click!/i });
+  expect(colorButton).toHaveStyle("backgroundColor: red");
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle("backgroundColor: blue");
 });
